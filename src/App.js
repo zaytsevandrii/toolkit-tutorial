d8f1166aed2440e12bf0252207587ef571a6ef30
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-26 15:05:59 +0000

    add thunk and fetch api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import { useSelector } from "react-redux"
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import "./App.css"
 import CartContainer from "./components/CartContainer"
 import Modal from "./components/Modal"
 import Navbar from "./components/Navbar"
+import { getCartItems } from "./features/cart/cartSlice"
+
 
 function App() {
+    const {isLodaing} =useSelector(store=>store.cart)
     const {isOpen}=useSelector(state=>state.modal)
+    const dispatch=useDispatch()
+
+    useEffect(()=>{
+        dispatch(getCartItems())
+    },[])
+
+    if(isLodaing){
+        return(
+            <div className="loading">
+                <h1>Loading...</h1>
+            </div>
+        )
+    }
     return (
         <main>
             {isOpen&&<Modal/>}
